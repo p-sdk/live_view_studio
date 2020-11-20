efe1923f7f31581ca7bd0076d16d7a2eed915b68
p-sdk <paul.sdk@icloud.com>
2020-11-20 22:12:21 +0100

    Singluarize "seats" when the value is 1

--- lib/live_view_studio_web/live/license_live.ex
+++ lib/live_view_studio_web/live/license_live.ex
@@ -3,6 +3,7 @@ defmodule LiveViewStudioWeb.LicenseLive do
 
   alias LiveViewStudio.Licenses
   import Number.Currency
+  import Inflex
 
   def mount(_params, _session, socket) do
     socket = assign(socket, seats: 3, amount: Licenses.calculate(3))
@@ -19,7 +20,7 @@ defmodule LiveViewStudioWeb.LicenseLive do
             <img src="images/license.svg">
             <span>
               Your license is currently for
-              <strong><%= @seats %></strong> seats.
+              <strong><%= @seats %></strong> <%= inflect("seats", @seats) %>.
             </span>
           </div>
 
