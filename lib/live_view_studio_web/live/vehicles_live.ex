3c16c98318d342408a0bbd742d43372926825793
p-sdk <paul.sdk@icloud.com>
2020-11-28 16:29:47 +0100

    Sort vehicles

--- lib/live_view_studio_web/live/vehicles_live.ex
+++ lib/live_view_studio_web/live/vehicles_live.ex
@@ -2,21 +2,38 @@ defmodule LiveViewStudioWeb.VehiclesLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Vehicles
+  import LiveViewStudioWeb.ParamHelpers,
+    only: [param_to_integer: 2, param_or_first_permitted: 3]
+
+  @permitted_sort_bys ~w(id make model color)
+  @permitted_sort_orders ~w(asc desc)
 
   def mount(_params, _session, socket) do
     {:ok, socket, temporary_assigns: [vehicles: []]}
   end
 
   def handle_params(params, _url, socket) do
-    page = String.to_integer(params["page"] || "1")
-    per_page = String.to_integer(params["per_page"] || "10")
-
+    page = param_to_integer(params["page"], 1)
+    per_page = param_to_integer(params["per_page"], 10)
     paginate_options = %{page: page, per_page: per_page}
-    vehicles = Vehicles.list_vehicles(paginate: paginate_options)
+
+    sort_by =
+      params
+      |> param_or_first_permitted("sort_by", @permitted_sort_bys)
+      |> String.to_atom()
+
+    sort_order =
+      params
+      |> param_or_first_permitted("sort_order", @permitted_sort_orders)
+      |> String.to_atom()
+
+    sort_options = %{sort_by: sort_by, sort_order: sort_order}
+
+    vehicles = Vehicles.list_vehicles(paginate: paginate_options, sort: sort_options)
 
     socket =
       assign(socket,
-        options: paginate_options,
+        options: Map.merge(paginate_options, sort_options),
         vehicles: vehicles
       )
 
@@ -31,23 +48,54 @@ defmodule LiveViewStudioWeb.VehiclesLive do
             socket,
             __MODULE__,
             page: socket.assigns.options.page,
-            per_page: per_page
+            per_page: per_page,
+            sort_by: socket.assigns.options.sort_by,
+            sort_order: socket.assigns.options.sort_order
           )
       )
 
     {:noreply, socket}
   end
 
-  defp pagination_link(socket, text, page, per_page, class) do
+  defp pagination_link(socket, text, page, options, class) do
     live_patch(text,
       to:
         Routes.live_path(
           socket,
           __MODULE__,
           page: page,
-          per_page: per_page
+          per_page: options.per_page,
+          sort_by: options.sort_by,
+          sort_order: options.sort_order
         ),
       class: class
     )
   end
+
+  defp sort_link(socket, text, sort_by, options) do
+    text =
+      if sort_by == options.sort_by do
+        text <> emoji(options.sort_order)
+      else
+        text
+      end
+
+    live_patch(text,
+      to:
+      Routes.live_path(
+        socket,
+        __MODULE__,
+        sort_by: sort_by,
+        sort_order: toggle_sort_order(options.sort_order),
+        page: options.page,
+        per_page: options.per_page
+      )
+    )
+  end
+
+  defp toggle_sort_order(:asc), do: :desc
+  defp toggle_sort_order(:desc), do: :asc
+
+  defp emoji(:asc), do: "👇"
+  defp emoji(:desc), do: "👆"
 end
