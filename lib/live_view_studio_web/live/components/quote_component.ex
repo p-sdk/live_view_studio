44873711a5f128763b6d9bfd5c9529ce7963166f
p-sdk <paul.sdk@icloud.com>
2020-12-03 16:01:55 +0100

    Colorize the quote

--- lib/live_view_studio_web/live/components/quote_component.ex
+++ lib/live_view_studio_web/live/components/quote_component.ex
@@ -4,7 +4,7 @@ defmodule LiveViewStudioWeb.QuoteComponent do
   import Number.Currency
 
   def mount(socket) do
-    {:ok, assign(socket, hrs_until_expires: 24)}
+    {:ok, assign(socket, hrs_until_expires: 24, color: "indigo")}
   end
 
   # def update(assigns, socket) do
@@ -14,11 +14,11 @@ defmodule LiveViewStudioWeb.QuoteComponent do
 
   def render(assigns) do
     ~L"""
-    <div class="text-center p-6 border-4 border-dashed border-indigo-600">
+    <div class="text-center p-6 border-4 border-dashed border-<%= @color %>-600">
       <h2 class="text-2xl mb-2">
         Our Best Deal:
       </h2>
-      <h3 class="text-xl font-semibold text-indigo-600">
+      <h3 class="text-xl font-semibold text-<%= @color %>-600">
         <%= @weight %> pounds of <%= @material %>
         for <%= number_to_currency(@price) %>
       </h3>
